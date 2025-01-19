f9df90496747f7bfd5f06107df6eb91c48a665ee
Amos Yeo <amosyeo.sh@gmail.com>
2025-01-20 00:26:12 +0800

    finished hw

--- src/components/Bookshelf/Bookshelf.jsx
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -2,14 +2,47 @@ import { useState } from 'react';
 
 
 const Bookshelf = () =>{
+    const [books, setBooks] = useState([
+        { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+        { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+    ]);
+
+    const [newBook, setNewBook] = useState({
+        title: '', author: ''
+    })
+
+    const handleInputChange = (event) => {
+        console.log(`Test handleInputChange`)
+        setNewBook({...newBook, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Test handleSubmit`)
+        setBooks([...books, newBook])
+        setNewBook({title: '', author:''})
+    }
 
     return(
         <div className="bookshelfDiv">
-        <div className="formDiv">
-            <h3>Add a Book</h3>
-            {/* Form will go here */}
-        </div>
-        <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+            <div className="formDiv">
+                <h3>Add a Book</h3>
+                <form onSubmit={handleSubmit}>
+                    <label>Title :</label>
+                    <input id="title" name="title" value={newBook.title} onChange={handleInputChange}></input>
+                    <label>Author: </label>
+                    <input id="author" name="author" value={newBook.author} onChange={handleInputChange}></input>
+                    <button type="submit" >Add book</button>
+                </form>
+            </div>
+            <div className="bookCardsDiv">
+                {books.map((el,index) => (
+                    <div key={index}>
+                        <h3>Title: {el.title}</h3>
+                        <div>Author: {el.author}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
